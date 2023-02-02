f9bc6cd124bccc8430eef6e027a6db3a0b291622
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2023-02-02 23:00:37 +0300

    feat: add tailwind settings to the project

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,11 @@
-const Home = () => {
-  return <h1>Hello world!</h1>
+import styles from '../styles/Home.module.css';
+
+const Registration = () => {
+  return <div className={styles.wrapper}>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
 }
 
-export default Home;
+export default Registration;
