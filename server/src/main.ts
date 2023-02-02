6b2e45898adced8a282087977dc8c2265894aef3
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2023-02-02 20:31:37 +0300

    feat: add user module

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+dotenv.config();
+
 async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT, () => console.log(`Server started on ${PORT} port`));
 }
 bootstrap();
