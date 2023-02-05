db7bbed673587f467263269826b3fce0fd670524
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2023-02-05 05:34:59 +0300

    fix: change create-user dto

--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -1,16 +1,25 @@
-import {IsString} from 'class-validator';
+import {IsNotEmpty, IsString} from 'class-validator';
 
 export class CreateUserDto {
+    @IsNotEmpty({
+        message: 'Nickname is a required field'
+    })
     @IsString({
         message: 'Nickname must be a string'
     })
     readonly nickname: string;
 
+    @IsNotEmpty({
+        message: 'Email is a required field'
+    })
     @IsString({
         message: 'Email must be a string'
     })
     readonly email: string;
 
+    @IsNotEmpty({
+        message: 'password is a required field'
+    })
     @IsString({
         message: 'Password must be a string'
     })
